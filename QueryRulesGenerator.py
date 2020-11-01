24c18ee1e4ba1dd66ea6e1048812e122ec0753d7
arjmandi <arjmandi.re@gmail.com>
2020-11-01 10:51:28 +0330

    1 issue has been fixed

--- QueryRulesGenerator.py
+++ QueryRulesGenerator.py
@@ -1,3 +1,5 @@
+import math
+
 class QueryRulesGenerator:
 
     def __make_query_file_name__(self, with_FLAGIN_FLAGOUT, no_backend):
@@ -8,45 +10,47 @@ class QueryRulesGenerator:
         return query_file_name
 
     def __generate_query_rules__(self, number_of_shards, query_file_name, no_backend):
+        columns = "(active, match_pattern, apply, destination_hostgroup, OK_msg)"
+        destination_hostgroup = 1
+        ok_msg = f'""' if no_backend else 'NULL'
         with open(query_file_name, "w") as query_file:
             query_file.write("delete from mysql_query_rules;\n")
             for i in range(number_of_shards):
-                destination_hostgroup = 1 if no_backend else 0
-                ok_msg = f'"Shard {i:03}"' if no_backend else 'NULL'
-                query_file.write(f"insert into mysql_query_rules (active, match_pattern, apply, destination_hostgroup, OK_msg) values (1, '\/\* {i:03}', 1, {destination_hostgroup}, {ok_msg});\n")
+                values = f"values (1, '\/\* {i:03}', 1, {destination_hostgroup}, {ok_msg})"
+                query_file.write(f"insert into mysql_query_rules {columns} {values};\n")
             query_file.write("LOAD MYSQL QUERY RULES TO RUNTIME;\n")
 
-    def __make_leaf__(self, query_file, _range, flag_in, flag_out, ok_msg, destination_hostgroup):
-        query_file.write(f"insert into mysql_query_rules (active,match_pattern,flagIN,flagOUT,apply,destination_hostgroup, OK_msg) values (1,'\/\* [{_range[0]}-{_range[1]}]',{flag_in},{flag_out},0,{destination_hostgroup}, {ok_msg});\n")
-        query_file.write(f"insert into mysql_query_rules (active,match_pattern,flagIN,flagOUT,apply,destination_hostgroup, OK_msg) values (1,'\/\* {_range[0]}' ,{flag_in}, 0,1, {destination_hostgroup}, {ok_msg});\n")
-        query_file.write(f"insert into mysql_query_rules (active,match_pattern,flagIN,flagOUT,apply,destination_hostgroup, OK_msg) values (1,'\/\* {_range[1]}' ,{flag_in}, 0,1, {destination_hostgroup}, {ok_msg});\n")
+    def __make_B_tree__(self, _range, query_file, ok_msg, destination_hostgroup, flag_in, flag_out):
+        if _range[1] - _range[0] == 0:
+            return
 
-    def __make_node__(self, query_file, _range, flag_in, flag_out, height, ok_msg, destination_hostgroup):
-        self.__make_leaf__(query_file, _range, flag_in, flag_out, ok_msg, destination_hostgroup)
-        if height == 0:
-            query_file.write(f"insert into mysql_query_rules (active,match_pattern,flagIN,flagOUT,apply,destination_hostgroup, OK_msg) values (1,'\/\* {_range[1] + 1}' , {flag_in}, 0,1, {destination_hostgroup}, {ok_msg});\n")
-            query_file.write(f"insert into mysql_query_rules (active,match_pattern,flagIN,flagOUT,apply,destination_hostgroup, OK_msg) values (1,'\/\* {_range[1] + 2}' , {flag_in}, 0,1, {destination_hostgroup}, {ok_msg});\n")
-            return _range[1] + 2
-
-        for i in range(height):
-            _range = [_range[1] + 1, _range[1] + 2]
-            return self.__make_node__(query_file, _range, flag_in, flag_out, height - 1, ok_msg, destination_hostgroup)
-
+        columns = "(active,match_pattern,flagIN,flagOUT,apply,destination_hostgroup, OK_msg)"
+        if _range[1] - _range[0] == 1:
+            match_pattern = f"'\/\* {_range[0]}'"
+            values = f"values (1, {match_pattern}, {flag_in}, 0,1, {destination_hostgroup}, {ok_msg})"
+            query_file.write(f"insert into mysql_query_rules {columns} {values};\n")
+            
+            match_pattern = f"'\/\* {_range[1]}'"
+            values = f"values (1, {match_pattern}, {flag_in}, 0,1, {destination_hostgroup}, {ok_msg})"
+            query_file.write(f"insert into mysql_query_rules {columns} {values};\n")
+            return
+        
+        match_pattern = f"'\/\* [{_range[0]}-{_range[1]}]'"
+        values = f"values (1, {match_pattern}, {flag_in},{flag_out},0,{destination_hostgroup}, {ok_msg})"
+        query_file.write(f"insert into mysql_query_rules {columns} {values};\n")
+        self.__make_B_tree__([_range[0], math.floor((_range[0] + _range[1]) / 2)], query_file, ok_msg, destination_hostgroup, flag_out, flag_out+1)
+        self.__make_B_tree__([math.floor((_range[0] + _range[1]) / 2) + 1, _range[1]], query_file, ok_msg, destination_hostgroup, flag_out, flag_out+1)
 
     def __generate_FALGIN_FLAGOUT_query_rules__(self, number_of_shards, query_file_name, no_backend):
-        _range = [0, 1]
-        height = 0
+        _range = [0, number_of_shards]
         flag_in = 0
         flag_out = 1
         ok_msg = '""' if no_backend else "NULL"
-        destination_hostgroup = "NULL" if no_backend else 0
+        destination_hostgroup = 1
+
         with open(query_file_name, "w") as query_file:
             query_file.write("delete from mysql_query_rules;\n")
-            while _range[1] <  number_of_shards:
-                _range[1] = self.__make_node__(query_file, _range, flag_in, flag_out, height, ok_msg, destination_hostgroup)
-                height += 1
-                flag_in = flag_out
-                flag_out += 1
+            self.__make_B_tree__(_range, query_file, ok_msg, destination_hostgroup, flag_in, flag_out)
             query_file.write("LOAD MYSQL QUERY RULES TO RUNTIME;\n")
 
     def generate(self, number_of_shards=200, with_FLAGIN_FLAGOUT=False, no_backend=False):
